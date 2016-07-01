63e91c1a4a66dec8be06666d85dc59a1bd998579
Sendy Halim <sendyhalim93@gmail.com>
2016-07-01 14:25:31 +0700

    Install tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build-essential git curl python-setuptools ruby vim emacs zsh
+sudo apt-get install build-essential git curl python-setuptools ruby vim emacs zsh tmux
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
