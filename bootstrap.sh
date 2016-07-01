05e71f87ffd78011f87f24da542ed4e9f7b8be9f
Sendy Halim <sendyhalim93@gmail.com>
2016-07-01 15:38:57 +0700

    Install python-dev

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build-essential git curl python-setuptools ruby vim emacs zsh tmux
+sudo apt-get install build-essential git curl python-setuptools python-dev ruby vim emacs zsh tmux
 
 # Good ol' terminal-style vim doesn't have access to the clipboard,
 # which is a part of the X windows system. Installing vim-gtk gives vim
